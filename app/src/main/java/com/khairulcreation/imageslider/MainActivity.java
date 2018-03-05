14f948019ac1bbfb911c7e5c8d05e6191546d229
khairulhelmie <30465528+khairulhelmie@users.noreply.github.com>
2018-03-05 16:16:52 +0800

    first commit

---
+++ app/src/main/java/com/khairulcreation/imageslider/MainActivity.java
@@ -0,0 +1,31 @@
+package com.khairulcreation.imageslider;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+}
+
+
+
